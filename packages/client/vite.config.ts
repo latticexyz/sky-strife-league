53843094727d0e6509b2f99f6ef8f8538318b4ae
Raymond Z <raykyri@gmail.com>
2023-12-03 19:18:25 -0500

    packages/client: let rollup bundle skystrife contracts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
@@ -13,5 +13,8 @@ export default defineConfig({
     target: "es2022",
     minify: true,
     sourcemap: true,
+    rollupOptions: {
+      external: [/^contracts\//, /^contracts-skystrife\//],
+    },
   },
-});
+})
